8109a52521fad2b62f274e7a8bedf13090d847ff
Test <abedinire1990@gmail.com>
2024-05-27 12:47:41 +0430

    Ddta Type

---
+++ src/Lecture2.java
@@ -0,0 +1,29 @@
+import java.sql.SQLOutput;
+
+public class Lecture2 {
+    public static void main(String[] args) {
+        String Name="Reyhane Abedini";
+        byte Age=33;
+        short BirthYear=1990;
+        int NationalCode=0011223344;
+        long AccountNumber=1009997788;
+        float Grade=18.340f;
+        double d1=31313.131332323;
+        boolean Work=true;
+        char Rey='r';
+
+
+        System.out.println("Name: "+Name);
+        System.out.println("Age: "+Age);
+        System.out.println("BirthYear: "+BirthYear);
+        System.out.println("NationalCode: "+NationalCode);
+        System.out.println("AccountNumber: "+AccountNumber);
+        System.out.println("Grade: "+Grade);
+        System.out.println("d1: "+d1);
+        System.out.println("Work: "+Work);
+        System.out.println("Rey: "+Rey);
+
+
+
+    }
+}
