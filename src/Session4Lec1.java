a1ac575822ce7ea84a364b278622b0dd528cf188
Test <abedinire1990@gmail.com>
2024-06-18 10:16:30 +0430

    Session 4 - Lecture 1

---
+++ src/Session4Lec1.java
@@ -0,0 +1,36 @@
+import java.sql.SQLOutput;
+
+public class Session4Lec1 {
+    public static void main(String[] args) {
+
+
+         /*   String age="25";
+        String aBool="true";
+
+        Integer ageInInteger=Integer.valueOf(age)+10;
+      //  Long 1=Long.valueOf(age);
+        Boolean aBooleanInBoolean=Boolean.parseBoolean(aBool);
+
+*/
+
+    String EE="1234567890";
+    byte Age=33;
+    short BirthYear=1990;
+    int NationalCode=0011223344;
+    long AccountNumber=1009997788;
+    float Grade=18.340f;
+    double d1=31313.131332323;
+    boolean Work=true;
+    char Rey='r';
+
+        System.out.println(String.valueOf(NationalCode));
+        System.out.println(String.valueOf(Rey));
+        System.out.println(String.valueOf(d1));
+        System.out.println(String.valueOf(AccountNumber));
+        System.out.println(String.valueOf(Grade));
+        System.out.println(String.valueOf(Work));
+        System.out.println(Integer.valueOf(EE));
+}
+    }
+
+
